4453f18469317599c74bde9f35989f9db21ef97c
Lipetka <liptak2000@gmail.com>
2022-06-15 18:29:52 +0200

    Problem 1 part 2

--- Problem 1/main.c
+++ Problem 1/main.c
@@ -27,8 +27,8 @@ void main() {
 	*/
 
 	for (int i = 0; i < sizeof(input) / sizeof(int); i++) { // cycle through all input values
-		int A = input[i+2] + input[i + 1] + input [i]; // calculate value of first group
-		int B = input[i +3] + input[i + 2] + input[i + 1]; // calculate value of second group
+		int A = input[i + 2] + input[i + 1] + input [i]; // calculate value of first group
+		int B = input[i + 3] + input[i + 2] + input[i + 1]; // calculate value of second group
 
 		if (B > A) { // if second ground is larger, increase number of times the depth has decreased
 			decreaseTimes++; 
