7f69d7a3f8d5783f1e1d5ff56a60d2586de538a3
Lipetka <liptak2000@gmail.com>
2022-06-15 18:27:28 +0200

    Solution to problem 1 part 2

--- Problem 1/main.c
+++ Problem 1/main.c
@@ -1,57 +1,41 @@
-// problem
-/*
-
-Count the number of times the next measurement is deeper than previous one
-
-Example of input:
-
-199
-200
-208
-210
-200
-207
-240
-269
-260
-263
-
-Example of calculation from given input:
-
-199 (N/A - no previous measurement)
-200 (increased)
-208 (increased)
-210 (increased)
-200 (decreased)
-207 (increased)
-240 (increased)
-269 (increased)
-260 (decreased)
-263 (increased)
-
-Example of output:
-
-The depth has increased 7 times
-*/
-
-
 // includes
 #include <stdio.h>
 #include "input.h"
 
+// inits
+
+
 // variables
 
-int decreasedTimes; // variable that stores number of times the depth has decreased
+int decreaseTimes = 0;
+
+//int decreasedTimes; // variable that stores number of times the depth has decreased
 
 
 // main code
 void main() {
 	
+	/*
+	
 	for (int i = 0; i < sizeof(input) / sizeof(int); i++) { // cycle through all input values
 		if (input[i + 1] > input[i]) { // if next input is bigger add 1 to decrease counter
-			decreasedTimes++;
+			decreaseTimes++;
 		}
 	}
-	printf("%d", decreasedTimes); // print number of times the depth has decreased
+	printf("%d", decreaseTimes); // print number of times the depth has decreased
+
+	*/
+
+	for (int i = 0; i < sizeof(input) / sizeof(int); i++) { // cycle through all input values
+		int A = input[i+2] + input[i + 1] + input [i]; // calculate value of first group
+		int B = input[i +3] + input[i + 2] + input[i + 1]; // calculate value of second group
+
+		if (B > A) { // if second ground is larger, increase number of times the depth has decreased
+			decreaseTimes++; 
+		}
+	}
+
+	printf("%d", decreaseTimes++);
 
  }
+
